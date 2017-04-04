c5e63cbe5c0de44d0dd0a6c93882405693b105d3
AnJeongSeop <ajs905@naver.com>
2017-04-04 16:08:16 +0900

    Cylinder클래스
    Cylinder클래스 5번

--- javaOO/Circle2.java
+++ javaOO/Circle2.java
@@ -6,12 +6,10 @@ public class Cylinder {
 		this.circle = circle;
 		this.height = height;
 	}
-
 	double getVolume() {
 		double Volume = circle.getArea() * height;
 		return Volume;
 	}
-
 	public static void main(String[] args) {
 		Cylinder cd = new Cylinder(new Circle(2.8), 5.6);
 		System.out.println("원통의 부피 = " + cd.getVolume());
