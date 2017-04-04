d9b9a16f2de94b8c7bb0b958fbb82b2383889771
AnJeongSeop <ajs905@naver.com>
2017-04-04 13:36:44 +0900

    프로그래밍 연습
    1번

---
+++ javaOO/Student.java
@@ -0,0 +1,13 @@
+public class Student {
+	String Stu;
+	int StuNum;
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Student a = new Student();
+		a.Stu = "컴퓨터 정보 공학과";
+		a.StuNum = 20141649;
+		System.out.println("학과 = " + a.Stu);
+		System.out.println("학번 = " + a.StuNum);
+	}
+}
