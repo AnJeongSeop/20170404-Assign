3c7ac4954b76f663129a018bfa2322db68901df4
AnJeongSeop <ajs905@naver.com>
2017-04-04 16:07:06 +0900

    Cylinder 클래스1
    4번 문제

--- javaOO/Circle2.java
+++ javaOO/Circle2.java
@@ -1,29 +1,19 @@
-public class Circle {
-	public double radius;
-	public static double PI = 3.141592;
-	
-	//생성자 구현
-	public Circle(double radius){
-		this.radius = radius;
-	}
-	//현재 반지름을 사용하여 원의 면적을 구하는 메소드
-	public double getArea() {
-		return radius * radius * PI;
-	}
-}
-class Cylinder {	
-	Circle circle; 
+public class Cylinder {
+	Circle circle;
 	double height;
-	public Cylinder(Circle circle, double height){
+
+	public Cylinder(Circle circle, double height) {
 		this.circle = circle;
 		this.height = height;
 	}
-	double getVolume(){		
+
+	double getVolume() {
 		double Volume = circle.getArea() * height;
 		return Volume;
 	}
+
 	public static void main(String[] args) {
-		Cylinder cylinder = new Cylinder(new Circle(2.8), 5.6);
-		System.out.println("원통의 부피 = " + cylinder.getVolume());
+		Cylinder cd = new Cylinder(new Circle(2.8), 5.6);
+		System.out.println("원통의 부피 = " + cd.getVolume());
 	}
 }
