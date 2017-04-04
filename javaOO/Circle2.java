30fe2e89861546f3b94a21155ee5bf2586b7d295
AnJeongSeop <ajs905@naver.com>
2017-04-04 14:58:30 +0900

    4번 프로그래밍 연습
    4번 문제

---
+++ javaOO/Circle2.java
@@ -0,0 +1,29 @@
+public class Circle {
+	public double radius;
+	public static double PI = 3.141592;
+	
+	//생성자 구현
+	public Circle(double radius){
+		this.radius = radius;
+	}
+	//현재 반지름을 사용하여 원의 면적을 구하는 메소드
+	public double getArea() {
+		return radius * radius * PI;
+	}
+}
+class Cylinder {	
+	Circle circle; 
+	double height;
+	public Cylinder(Circle circle, double height){
+		this.circle = circle;
+		this.height = height;
+	}
+	double getVolume(){		
+		double Volume = circle.getArea() * height;
+		return Volume;
+	}
+	public static void main(String[] args) {
+		Cylinder cylinder = new Cylinder(new Circle(2.8), 5.6);
+		System.out.println("원통의 부피 = " + cylinder.getVolume());
+	}
+}
